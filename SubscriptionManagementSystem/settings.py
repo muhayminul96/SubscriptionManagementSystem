941a3cb6d41bbae673ecabfe2ddf33d41a74dc29
muhayminul96 <muhayminul96@gmail.com>
2025-07-30 15:45:39 +0600

    added admin panel

--- SubscriptionManagementSystem/settings.py
+++ SubscriptionManagementSystem/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.admin',
     'django.contrib.staticfiles',
     'rest_framework_simplejwt',
     'rest_framework',
--- subscription_app/admin.py
+++ subscription_app/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
+from .models import Subscription, ExchangeRateLog, Plan
 # Register your models here.
