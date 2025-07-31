d21c97877f3c7f73db7e2c3f4666b97d009f6bdd
muhayminul96 <muhayminul96@gmail.com>
2025-07-31 15:05:54 +0600

    added subcriptions model to admin

--- subscription_app/admin.py
+++ subscription_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 from .models import Subscription, ExchangeRateLog, Plan
 # Register your models here.
+@admin.register(ExchangeRateLog)
+class ExchangeRateLogAdmin(admin.ModelAdmin):
+    list_display = ('base_currency', 'target_currency', 'rate', 'fetched_at')
+    list_filter = ('fetched_at',)
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('name','price_in_usd','duration_days')
+    list_filter = ('name',)
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('plan', 'user', 'start_date','status')
+    list_filter = ('start_date',)
